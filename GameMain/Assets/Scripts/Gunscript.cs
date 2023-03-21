89b6ced15fe9935b21c96332bbed29d9b3ea0775
Samurai15927 <samurai15927@gmail.com>
2023-03-22 09:56:15 +1300

    V2

---
+++ GameMain/Assets/Scripts/Gunscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunscript : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        transform.rotation = player.transform.rotation;
+
+
+  
+    }
+}
