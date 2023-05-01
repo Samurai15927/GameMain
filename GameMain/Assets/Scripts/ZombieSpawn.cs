8d0c9334ee7ef6c95d824e0d3730180cbcb2aae8
Samurai15927 <samurai15927@gmail.com>
2023-05-02 10:58:25 +1200

    V8

--- GameMain/Assets/Scripts/ZombieSpawn.cs
+++ GameMain/Assets/Scripts/ZombieSpawn.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class ZombieSpawn : MonoBehaviour
 {
-    public GameObject
-    public float spawnRate;
+    public float spawnRate = 4;
+    public Vector3 spawnArea = new Vector3(5f, 5f, 5f);
+    public int spawnCount = 5;
+    public int roundCount = 1;
+    GameObject ZombieRed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ZombieRed = GameObject.Find("ZombieRed");
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnArea.x, spawnArea.x), Random.Range(-spawnArea.y, spawnArea.y), Random.Range(-spawnArea.z, spawnArea.z));
+            Instantiate(ZombieRed, spawnPosition, Quaternion.identity);
+            StartCoroutine(SpawnObject());
+        }
         
+    }
+
+    IEnumerator SpawnObject()
+    { 
+        yield return new WaitForSeconds(5f);
     }
 }
