33c38886a5f16f7baf785373e7abcc0ab9d8abcc
Samurai15927 <samurai15927@gmail.com>
2023-05-01 13:27:58 +1200

    V7

---
+++ GameMain/Assets/Scripts/ZombieSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawn : MonoBehaviour
+{
+    public GameObject
+    public float spawnRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
