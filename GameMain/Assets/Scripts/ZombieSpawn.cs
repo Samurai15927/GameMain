1ab219c1b7c811ec9851e02a06a8b42f9330c6ca
Samurai15927 <samurai15927@gmail.com>
2023-05-03 09:56:31 +1200

    V10

--- GameMain/Assets/Scripts/ZombieSpawn.cs
+++ GameMain/Assets/Scripts/ZombieSpawn.cs
@@ -6,29 +6,38 @@ public class ZombieSpawn : MonoBehaviour
 {
     public float spawnRate = 4;
     public Vector3 spawnArea = new Vector3(5f, 5f, 5f);
-    public int spawnCount = 5;
+    public int spawnCount = 2;
     public int roundCount = 1;
-    GameObject ZombieRed;
+    public GameObject[] _zombieType;
+  
     // Start is called before the first frame update
+
     void Start()
     {
-        ZombieRed = GameObject.Find("ZombieRed");
+    
+        SpawnZombies();
     }
-
+        
     // Update is called once per frame
     void Update()
     {
+
+    }
+    void SpawnZombies()
+    {
         for (int i = 0; i < spawnCount; i++)
         {
-            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnArea.x, spawnArea.x), Random.Range(-spawnArea.y, spawnArea.y), Random.Range(-spawnArea.z, spawnArea.z));
-            Instantiate(ZombieRed, spawnPosition, Quaternion.identity);
-            StartCoroutine(SpawnObject());
-        }
-        
-    }
+            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnArea.x, spawnArea.x), spawnArea.y, Random.Range(-spawnArea.z, spawnArea.z));
+            Instantiate(_zombieType[Random.Range(0, _zombieType.Length)], spawnPosition, Quaternion.identity);
 
+        }
+        StartCoroutine(SpawnObject());
+    }
     IEnumerator SpawnObject()
     { 
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(20f);
+        roundCount++;
+        spawnCount += 2;
+        SpawnZombies();
     }
 }
