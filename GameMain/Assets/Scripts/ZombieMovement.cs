6c632e0b84d6f25869e0b925b731c0cae4be9450
Samurai15927 <samurai15927@gmail.com>
2023-05-08 13:23:53 +1200

    V12

--- GameMain/Assets/Scripts/ZombieMovement.cs
+++ GameMain/Assets/Scripts/ZombieMovement.cs
@@ -9,21 +9,37 @@ public class ZombieMovement : MonoBehaviour
     public float zombieSpeed;
     public GameObject zombie;
     public float distance;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         player = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform playerTransform = player.transform;
-        transform.LookAt(playerTransform);
-        Vector3 Rotation = transform.eulerAngles;
-        Rotation.x = 0f;
-        transform.eulerAngles = Rotation;
-        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * zombieSpeed);
         distance = Vector3.Distance(player.transform.position, zombie.transform.position);
+        if (distance < 1.5)
+        {
+            animator.SetBool("IsClose", true);
+            transform.LookAt(player.transform);
+            Vector3 Rotation = transform.eulerAngles;
+            Rotation.x = 0f;
+            transform.eulerAngles = Rotation;
+        }
+        else
+        {
+            animator.SetBool("IsClose", false);
+            Transform playerTransform = player.transform;
+            transform.LookAt(playerTransform);
+            Vector3 Rotation = transform.eulerAngles;
+            Rotation.x = 0f;
+            transform.eulerAngles = Rotation;
+            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * zombieSpeed);
+            
+        }
+       
     }
 }
