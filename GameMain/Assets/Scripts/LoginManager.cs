590eca2647ab4c4577304adc5c8947e9f22a8d9a
Samurai15927 <samurai15927@gmail.com>
2023-05-15 13:27:42 +1200

    v14

---
+++ GameMain/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField username;
+    public Button loginButton;
+    private string usernameText;
+    void Start()
+    {
+        usernameText = username.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void UsernameChecker()
+    {
+       
+    }
+    
+
+}  
+
