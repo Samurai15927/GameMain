498cade7ef5710ca205c62b2941a594dfa65da4c
Samurai15927 <samurai15927@gmail.com>
2023-05-16 10:59:06 +1200

    v15

--- GameMain/Assets/Scripts/LoginManager.cs
+++ GameMain/Assets/Scripts/LoginManager.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LoginManager : MonoBehaviour
 {
     public InputField username;
     public Button loginButton;
-    private string usernameText;
+    private int charCount;
+    public TMP_Text errorMessage;
+    private int numCount;
+
+
     void Start()
     {
-        usernameText = username.text;
+        
     }
 
     // Update is called once per frame
@@ -18,9 +23,39 @@ public class LoginManager : MonoBehaviour
     {
         
     }
-    void UsernameChecker()
+    public void UsernameChecker()
     {
-       
+        int specCount = username.text.Length;
+
+        foreach (char c in username.text)
+        {
+            if (char.IsDigit(c))
+            {
+                numCount++;
+
+            }
+            else if (char.IsLetterOrDigit(c))
+            {
+                specCount--;
+            }
+
+        }
+        if (charCount > 10 || charCount < 2)
+        {
+            errorMessage.SetText("The Name Must Have Between 2 And 10 Characters");
+        }
+        else if (numCount > 0)
+        {
+            errorMessage.SetText("No Numbers Are Allowed");
+        }
+        else if (specCount > 0)
+        {
+            errorMessage.SetText("No Special Characters allowed");
+        }
+        else 
+        {
+            errorMessage.SetText("Username Accepted");
+        }
     }
     
 
