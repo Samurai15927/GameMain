386db33e4b8190597ce62cf88af9ae5e122ac79a
Samurai15927 <samurai15927@gmail.com>
2023-05-29 13:27:29 +1200

    Update Gunscript.cs

--- GameMain/Assets/Scripts/Gunscript.cs
+++ GameMain/Assets/Scripts/Gunscript.cs
@@ -4,30 +4,56 @@ using UnityEngine;
 
 public class Gunscript : MonoBehaviour
 {
+
     // Update is called once per frame
+  
+    private bool isShooting = false;
+   
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
-            {
-                if (hit.collider != null)
-                {
-                    // Check if the hit object is a ZombieRed or ZombieGreen
-                    if (hit.collider.CompareTag("ZombieRed") || hit.collider.CompareTag("ZombieGreen"))
-                    {
-                        // Get the ZombieHealth component of the hit object
-                        ZombieHealth zombieHealth = hit.collider.GetComponent<ZombieHealth>();
+            isShooting = true;
+            StartCoroutine(ShootRepeatedly());
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isShooting = false;
+        }
+    }
 
-                        // Apply damage to the zombie
-                        if (zombieHealth != null)
-                        {
-                            zombieHealth.TakeDamage();
-                        }
+    IEnumerator ShootRepeatedly()
+    {
+        while (isShooting)
+        {
+            Shoot();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100))
+        {
+            if (hit.collider != null)
+            {
+                // Check if the hit object is a ZombieRed or ZombieGreen
+                if (hit.collider.CompareTag("ZombieRed") || hit.collider.CompareTag("ZombieGreen"))
+                {
+                    // Get the ZombieHealth component of the hit object
+                    ZombieHealth zombieHealth = hit.collider.GetComponent<ZombieHealth>();
+
+                    // Apply damage to the zombie
+                    if (zombieHealth != null)
+                    {
+                        zombieHealth.TakeDamage();
                     }
                 }
             }
         }
+       
+
     }
+
 }
+
