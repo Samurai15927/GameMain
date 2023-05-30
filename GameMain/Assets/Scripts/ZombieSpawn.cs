6df7a7b2063c0c834c4804b42390bb8c19f2959e
Samurai15927 <samurai15927@gmail.com>
2023-05-31 09:59:10 +1200

    V22

--- GameMain/Assets/Scripts/ZombieSpawn.cs
+++ GameMain/Assets/Scripts/ZombieSpawn.cs
@@ -5,34 +5,55 @@ using UnityEngine;
 public class ZombieSpawn : MonoBehaviour
 {
     public float spawnRate = 4;
-    public Vector3 spawnArea = new Vector3(5f, 5f, 5f);
+    public Vector3 spawnArea = new Vector3(9f, 0f, 9f);
     public int spawnCount = 2;
     public GameObject[] _zombieType;
-  
-    // Start is called before the first frame update
+
+    public Vector3 spawnPosition;
+    private GameObject player;
 
     void Start()
     {
-           SpawnZombies();
+        player = GameObject.Find("Player"); // Adjust the game object name if needed
+        SpawnZombies();
     }
-        
-    // Update is called once per frame
+
     void Update()
     {
 
     }
+
     void SpawnZombies()
     {
         for (int i = 0; i < spawnCount; i++)
         {
-            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnArea.x, spawnArea.x), spawnArea.y, Random.Range(-spawnArea.z, spawnArea.z));
+            Vector3 spawnPosition = ValidSpawnPosition();
             Instantiate(_zombieType[Random.Range(0, _zombieType.Length)], spawnPosition, Quaternion.identity);
-
         }
         StartCoroutine(SpawnObject());
     }
+
+    Vector3 ValidSpawnPosition()
+    {
+        bool validPosition = false;
+        float playerRadius = 4f; // Adjust this value based on the size of your player's collider or model
+
+        while (!validPosition)
+        {
+            spawnPosition = transform.position + new Vector3(Random.Range(-spawnArea.x, spawnArea.x), spawnArea.y, Random.Range(-spawnArea.z, spawnArea.z));
+
+            if (Vector3.Distance(spawnPosition, player.transform.position) > playerRadius)
+            {
+                validPosition = true;
+            }
+        }
+
+        Debug.Log(spawnPosition); // Output the spawn position for debugging
+        return spawnPosition;
+    }
+
     IEnumerator SpawnObject()
-    { 
+    {
         yield return new WaitForSeconds(20f);
         spawnCount += 2;
         SpawnZombies();
